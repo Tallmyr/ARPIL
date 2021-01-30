0f5bb949bfe487f20eff1fc38aaf5890ed6983c6
Simon Tallmyr <simon@tallmyr.se>
2021-01-30 23:57:08 +0000

    Added BMW to installer

--- scriptmodules/ports/Arpil.sh
+++ scriptmodules/ports/Arpil.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This is an unofficial addon script to the RetroPie Project. This script has not been approved by 
-# the RetroPie team, and is not an official package.
+# the RetroPie team, and is not an official package. 
 
 rp_module_id="arpil"
 rp_module_desc="Archipelago for RetroPie Launcher"
@@ -9,19 +9,29 @@ rp_module_licence="MIT https://github.com/Tallmyr/ARPIL/blob/main/LICENCE"
 rp_module_help="Copy A Link to the Past Japan 1.0 ROM to $romdir/ports/Arpil"
 rp_module_section="exp"
 
+function depends_arpil() {
+    getDepends jq python3 python3-pip
+}
+
 function sources_arpil() {
-    gitPullOrClone "$md_build" "https://github.com/Tallmyr/ARPIL" "dev"
+    gitPullOrClone "$md_build/arpil" "https://github.com/Tallmyr/ARPIL" "dev"
+    commit=$(curl 'https://api.github.com/repos/Berserker66/MultiWorld-Utilities/tags?per_page=1' | jq -r '.[0].commit[0].sha')
+    gitPullOrClone "$md_build/berserker" "https://github.com/Berserker66/MultiWorld-Utilities" "main" "$commit"
 }
 
 function install_arpil() {
     md_ret_files=(
-        'LICENCE'
-        'launcher.sh'
+        'arpil/LICENCE'
+        'arpil/launcher.sh'
+        'berserker'
     )
 }
 
 function configure_arpil() {
     addPort "$md_id" "arpil" "Arpil" "$md_inst/launcher.sh"
+    chmod +x "$md_inst/launcher.sh"
+    pip3 install -r "$md_inst/berserker/requirements.txt"
 
-    [[ "$md_mode" != "install" ]] && return
+    [[ "$md_mode" = "install" ]] && return
+
 }
